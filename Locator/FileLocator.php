8b8ddc5c444c8c2d1deefbf6921d3d21651c18b5
David Sanchez <david38sanchez@gmail.com>
2019-02-11 18:23:16 -0500

    Override `FileLocator` from LiipThemeBundle. Get theme path formatted as `vendor/name` from Theme class

---
+++ Locator/FileLocator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Harmony\Bundle\ThemeBundle\Locator;
+
+use Harmony\Bundle\CoreBundle\Component\HttpKernel\AbstractKernel;
+use Liip\ThemeBundle\Locator\FileLocator as BaseFileLocator;
+
+/**
+ * Class FileLocator
+ *
+ * @package Harmony\Bundle\ThemeBundle\Locator
+ */
+class FileLocator extends BaseFileLocator
+{
+
+    /**
+     * Locate Resource Theme aware. Only working for app/Resources.
+     *
+     * @param string $name
+     * @param string $dir
+     * @param bool   $first
+     *
+     * @return string|array
+     */
+    protected function locateAppResource($name, $dir = null, $first = true)
+    {
+        /** @var AbstractKernel $kernel */
+        $kernel = $this->kernel;
+        if ($this->kernel instanceof AbstractKernel) {
+            $themeName = $this->activeTheme->getName();
+            if (isset($kernel->getThemes()[$themeName])) {
+                $theme           = $kernel->getThemes()[$themeName];
+                $this->lastTheme = implode('/', array_slice(explode(DIRECTORY_SEPARATOR, $theme->getPath()), - 2, 2));
+            }
+        }
+
+        return parent::locateAppResource($name, $dir, $first);
+    }
+}
