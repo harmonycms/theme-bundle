84321d0de0c4aaec185617e67fc87e278a8ea005
David Sanchez <david.sanchez@nventive.com>
2019-02-14 16:07:06 -0500

    Unfinished work, add menu configuration and listener

---
+++ EventListener/MenuListener.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Harmony\Bundle\ThemeBundle\EventListener;
+
+use Harmony\Bundle\CoreBundle\Component\HttpKernel\AbstractKernel;
+use Helis\SettingsManagerBundle\Event\ConfigureMenuEvent;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+/**
+ * Class MenuListener
+ *
+ * @package Harmony\Bundle\ThemeBundle\EventListener
+ */
+class MenuListener
+{
+
+    /** @var AbstractKernel|KernelInterface $kernel */
+    protected $kernel;
+
+    /**
+     * MenuListener constructor.
+     *
+     * @param KernelInterface|AbstractKernel $kernel
+     */
+    public function __construct(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    /**
+     * @param ConfigureMenuEvent $event
+     */
+    public function onMenuConfigure(ConfigureMenuEvent $event)
+    {
+    }
+}
