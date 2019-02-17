432ca0b1bfb56fb89772353b0f3407a8c7accc99
David Sanchez <david38sanchez@gmail.com>
2019-02-17 17:34:07 -0500

    Optimize code to get asset url. Add logic to get asset from current active theme or parent theme

--- Twig/AssetsExtension.php
+++ Twig/AssetsExtension.php
@@ -2,17 +2,24 @@
 
 namespace Harmony\Bundle\ThemeBundle\Twig;
 
+use Harmony\Bundle\CoreBundle\Component\HttpKernel\AbstractKernel;
 use Harmony\Bundle\ThemeBundle\HarmonyThemeBundle;
+use Harmony\Sdk\Theme\ThemeInterface;
+use Helis\SettingsManagerBundle\Settings\SettingsRouter;
 use Liip\ThemeBundle\ActiveTheme;
 use Symfony\Bridge\Twig\Extension\AssetExtension as BridgeAssetExtension;
 use Symfony\Component\Asset\Packages;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Twig\Extension\GlobalsInterface;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
 
 /**
  * Class AssetsExtension
  *
  * @package Harmony\Bundle\ThemeBundle\Twig
  */
-class AssetsExtension extends BridgeAssetExtension
+class AssetsExtension extends BridgeAssetExtension implements GlobalsInterface
 {
 
     /** @var ActiveTheme $activeTheme */
@@ -21,18 +28,67 @@ class AssetsExtension extends BridgeAssetExtension
     /** @var string $projectDir */
     protected $projectDir;
 
+    /** @var KernelInterface|AbstractKernel $kernel */
+    protected $kernel;
+
+    /** @var SettingsRouter $settingsRouter */
+    protected $settingsRouter;
+
     /**
      * AssetsExtension constructor.
      *
-     * @param Packages    $packages
-     * @param ActiveTheme $activeTheme
-     * @param string      $projectDir
+     * @param KernelInterface $kernel
+     * @param Packages        $packages
+     * @param ActiveTheme     $activeTheme
+     * @param SettingsRouter  $settingsRouter
+     * @param string          $projectDir
      */
-    public function __construct(Packages $packages, ActiveTheme $activeTheme, string $projectDir)
+    public function __construct(KernelInterface $kernel, Packages $packages, ActiveTheme $activeTheme,
+                                SettingsRouter $settingsRouter, string $projectDir)
     {
         parent::__construct($packages);
-        $this->activeTheme = $activeTheme;
-        $this->projectDir  = $projectDir;
+        $this->kernel         = $kernel;
+        $this->activeTheme    = $activeTheme;
+        $this->projectDir     = $projectDir;
+        $this->settingsRouter = $settingsRouter;
+    }
+
+    /**
+     * Returns a list of functions to add to the existing list.
+     *
+     * @return TwigFunction[]
+     */
+    public function getFunctions(): array
+    {
+        return array_merge(parent::getFunctions(), [
+            new TwigFunction('asset_theme', [$this, 'getAssetUrl'])
+        ]);
+    }
+
+    /**
+     * Returns a list of filters to add to the existing list.
+     *
+     * @return TwigFilter[]
+     */
+    public function getFilters(): array
+    {
+        return array_merge(parent::getFilters(), [
+            new TwigFilter('theme', [$this, 'getThemeUrl'])
+        ]);
+    }
+
+    /**
+     * Returns a list of global variables to add to the existing list.
+     *
+     * @return array An array of global variables
+     */
+    public function getGlobals(): array
+    {
+        return [
+            'harmony' => [
+                'site_name' => $this->settingsRouter->get('site_name')
+            ]
+        ];
     }
 
     /**
@@ -47,11 +103,26 @@ class AssetsExtension extends BridgeAssetExtension
      */
     public function getAssetUrl($path, $packageName = null): string
     {
-        $url = sprintf('%s/%s/%s', HarmonyThemeBundle::THEMES_DIR, $this->activeTheme->getName(), $path);
-        if (is_file($this->projectDir . '/web/' . $url)) {
-            return parent::getAssetUrl($url, $packageName);
+        $theme = $this->kernel->getThemes()[$this->activeTheme->getName()];
+        /** @var ThemeInterface|null $parentTheme */
+        $parentTheme = null;
+        if (!empty($theme->getParent())) {
+            $parentTheme = $theme->getParent();
+            $parentTheme = new $parentTheme();
         }
 
-        return parent::getAssetUrl($path, $packageName);
+        $assetPath = sprintf('%s/%s/%s', HarmonyThemeBundle::THEMES_DIR, $theme->getShortName(), $path);
+
+        // Asset exists in current active theme
+        if (\file_exists(sprintf('%s/public/%s', $this->kernel->getProjectDir(), $assetPath))) {
+            return parent::getAssetUrl($assetPath, $packageName);
+        } // Has a parent theme
+        elseif (null !== $parentTheme && \file_exists(sprintf('%s/public/%s', $this->kernel->getProjectDir(),
+                $parentAssetPath = sprintf('%s/%s/%s', HarmonyThemeBundle::THEMES_DIR, $parentTheme->getShortName(),
+                    $path)))) {
+            return parent::getAssetUrl($parentAssetPath, $packageName);
+        }
+
+        return parent::getAssetUrl($assetPath, $packageName);
     }
 }
